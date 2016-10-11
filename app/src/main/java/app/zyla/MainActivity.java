3655d15968d04df87846d8e9ccdbcbd2d69b1252
Hugo Piso <hugopiso@yahoo.com>
2016-10-10 22:14:28 -0400

    example for calling todo list via webservice

--- app/src/main/java/app/zyla/MainActivity.java
+++ app/src/main/java/app/zyla/MainActivity.java
@@ -1,52 +1,108 @@
 package app.zyla;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView taskListView;
+    private String url = "https://jsonplaceholder.typicode.com/todos"; //todo change to real webservice (just testing)
+
+    ArrayList<HashMap<String, String>> taskList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        taskList = new ArrayList<>();
+        taskListView = (ListView) findViewById(R.id.task_list);
+
+        new GetTasks().execute();
+    }
+
+    /**
+     * Async task class to get json by making HTTP call
+     */
+    private class GetTasks extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            HttpHandler sh = new HttpHandler();
+            String jsonStr = sh.webServiceGetCall(url);
+
+            if (jsonStr != null) {
+                try {
+                    JSONArray tasks = new JSONArray(jsonStr);
+                    System.out.println(tasks);
+
+                    for (int i = 0; i < tasks.length(); i++) {
+                        JSONObject t = tasks.getJSONObject(i);
+                        String id    = t.getString("id");
+                        String title = t.getString("title");
+
+                        HashMap<String, String> task = new HashMap<>();
+                        task.put("id", id);
+                        task.put("title", title);
+
+                        // adding task to task list
+                        taskList.add(task);
+                    }
+                } catch (final JSONException e) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(),
+                                    "Json parsing error: " + e.getMessage(),
+                                    Toast.LENGTH_LONG)
+                                    .show();
+                        }
+                    });
+                }
             }
-        });
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            return null;
         }
 
-        return super.onOptionsItemSelected(item);
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+
+            /**
+             * Updating parsed JSON data into ListView
+             * */
+            ListAdapter adapter = new SimpleAdapter(
+                    MainActivity.this,
+                    taskList,
+                    R.layout.list_item,
+                    new String[] {"title"},
+                    new int[]{R.id.title}
+            );
+
+            taskListView.setAdapter(adapter);
+        }
+
     }
+
 }
