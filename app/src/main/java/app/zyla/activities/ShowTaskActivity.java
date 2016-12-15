f395a73da121673f4442c5e039faef5db2a73fad
Hugo Piso <hugopiso@yahoo.com>
2016-12-15 17:01:11 -0500

    update show task by adding percentage of time already used

--- app/src/main/java/app/zyla/activities/ShowTaskActivity.java
+++ app/src/main/java/app/zyla/activities/ShowTaskActivity.java
@@ -7,8 +7,14 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.CompoundButton;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.Switch;
 import android.widget.TextView;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 import app.zyla.R;
 import app.zyla.listener.isDoneEventListener;
@@ -46,5 +52,49 @@ public class ShowTaskActivity extends AppCompatActivity {
         TextView taskToShow = (TextView) findViewById(R.id.name);
         taskToShow.setText(task.getName());
 
+
+
+        ProgressBar pb = (ProgressBar) findViewById(R.id.progressBar);
+        pb.setMax(100);
+        pb.setProgress(10);
+
+        SimpleDateFormat dateFormat =
+                new SimpleDateFormat("yyyy-MM-dd hh:mm:ss", Locale.getDefault());
+        Date date = new Date();
+        String dateNow = dateFormat.format(date);
+
+
+        try {
+            Date dateCreation = dateFormat.parse(task.getCreationDate());
+            Date dateToday = dateFormat.parse(dateNow);
+            Date dateLimit = dateFormat.parse(task.getLimitDate() + " 00:00:00");
+
+            int diffBetweenCreationAndToday = this.getDifferenceInDay(dateCreation, dateToday);
+            int diffBetweenCreationAndLimit = this.getDifferenceInDay(dateCreation, dateLimit);
+            int evolutionInPercentage = (int) ((diffBetweenCreationAndToday * 100.0f) / diffBetweenCreationAndLimit);
+
+            TextView taskEvolution = (TextView) findViewById(R.id.task_evolution);
+            taskEvolution.setText(evolutionInPercentage + " %");
+            pb.setProgress(evolutionInPercentage);
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+
     }
+
+    public int getDifferenceInDay(Date startDate, Date endDate){
+
+        long different = endDate.getTime() - startDate.getTime();
+
+        Integer differentInt = (int) (long) different;
+        int secondsInMilli = 1000;
+        int minutesInMilli = secondsInMilli * 60;
+        int hoursInMilli = minutesInMilli * 60;
+        int daysInMilli = hoursInMilli * 24;
+
+        return differentInt / daysInMilli;
+    }
+
 }
