88cc30489b08fed27e3fa455bdac773316603be3
Hugo Piso <hugopiso@yahoo.com>
2016-12-15 23:33:59 -0500

    add score / rewards in menu

--- app/src/main/java/app/zyla/activities/MainActivity.java
+++ app/src/main/java/app/zyla/activities/MainActivity.java
@@ -7,6 +7,8 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ListView;
 import java.util.ArrayList;
@@ -80,5 +82,28 @@ public class MainActivity extends AppCompatActivity {
         });
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(mViewPager);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.score_rewards) {
+            System.out.println("rewards");
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
