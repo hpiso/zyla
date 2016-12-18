af1b5c90f216ba731a035636b76d901bafa9d671
Hugo Piso <hugopiso@yahoo.com>
2016-12-18 13:38:10 -0500

    add advise and delete btn on show task

--- app/src/main/java/app/zyla/activities/ShowTaskActivity.java
+++ app/src/main/java/app/zyla/activities/ShowTaskActivity.java
@@ -5,13 +5,18 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -20,33 +25,36 @@ import java.util.Locale;
 
 import app.zyla.R;
 import app.zyla.Service.Helper;
+import app.zyla.database.DatabaseHandler;
 import app.zyla.listener.isDoneEventListener;
 import app.zyla.models.Task;
 
 public class ShowTaskActivity extends AppCompatActivity {
 
     private Drawable bg;
+    private ShowTaskActivity showTaskActivity = this;
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Task task = (Task) getIntent().getSerializableExtra("taskToShow");
+        final Task task = (Task) getIntent().getSerializableExtra("taskToShow");
         setContentView(R.layout.show_task);
 
         LinearLayout layout = (LinearLayout) findViewById(R.id.layout_bg);
 
         switch (task.getCategory()) {
             case "Sport":
-                bg = getResources().getDrawable( R.drawable.bg_sport);
+                bg = getResources().getDrawable(R.drawable.bg_sport);
                 break;
             case "Study":
-                bg = getResources().getDrawable( R.drawable.bg_study);
+                bg = getResources().getDrawable(R.drawable.bg_study);
                 break;
             case "Cleaning":
-                bg = getResources().getDrawable( R.drawable.bg_cleaning);
+                bg = getResources().getDrawable(R.drawable.bg_cleaning);
                 break;
             case "Grocery":
-                bg = getResources().getDrawable( R.drawable.bg_grocery);
+                bg = getResources().getDrawable(R.drawable.bg_grocery);
                 break;
         }
 
@@ -70,6 +78,52 @@ public class ShowTaskActivity extends AppCompatActivity {
         TextView taskEvolution = (TextView) findViewById(R.id.task_evolution);
         taskEvolution.setText(evolutionInPercentage + " %");
         pb.setProgress(evolutionInPercentage);
+
+        Button adviseBtn = (Button) findViewById(R.id.advise_btn);
+
+        switch (task.getCategory()) {
+            case "Cleaning":
+                adviseBtn.setVisibility(View.GONE);
+                break;
+            case "Study":
+                adviseBtn.setVisibility(View.GONE);
+                break;
+        }
+
+        adviseBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                switch (task.getCategory()) {
+                    case "Sport":
+                        intent = new Intent(showTaskActivity, SportAdivseActivity.class);
+                        break;
+                    case "Grocery":
+                        intent = new Intent(showTaskActivity, MapsActivity.class);
+                        break;
+                }
+
+                showTaskActivity.startActivity(intent);
+            }
+        });
+
+
+        Button deleteBtn = (Button) findViewById(R.id.delete_btn);
+        deleteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatabaseHandler db = new DatabaseHandler(showTaskActivity);
+                db.deleteTask(task);
+
+                intent = new Intent(showTaskActivity, MainActivity.class);
+                showTaskActivity.startActivity(intent);
+
+                Toast toast = Toast.makeText(showTaskActivity, "Task deleted", Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
+
     }
 
 }
